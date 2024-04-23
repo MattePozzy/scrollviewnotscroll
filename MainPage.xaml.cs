10d39e15bcdc973cc804e11601b9c54cf01e0e40
Matteo <7420345+MattePozzy@users.noreply.github.com>
2024-04-23 16:01:37 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MauiApp6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public partial class MyViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private List<MyClass> myList;
+
+        public MyViewModel()
+        {
+            MyList = [];
+            for (int i = 0; i < 100; i++)
+            {
+                MyList.Add(new MyClass() { Name = $"Name_{i}", Surname = $"Surname_{i}" });
+            }
+        }
+    }
+
+    public class MyClass
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
